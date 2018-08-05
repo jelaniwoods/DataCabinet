67d1ff87160f4eb89d9c15fc3794cc4a378b991b
sun-vm <jmw686@nyu.edu>
2018-08-05 01:42:36 -0500

    associating user and docs

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_063637) do
+ActiveRecord::Schema.define(version: 2018_08_05_063907) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "context"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
